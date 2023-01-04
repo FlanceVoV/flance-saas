21bea2d6a57564d1e21107fef442980e069b276d
jianghf41372 <18151681814Jhf>
2023-01-04 13:51:42 +0800

    [新增]新特性 支持 字段根据实例对象新增db未配置的字段

--- flance-saas-db/src/main/java/com/flance/saas/db/tables/common/BaseTable.java
+++ flance-saas-db/src/main/java/com/flance/saas/db/tables/common/BaseTable.java
@@ -17,7 +17,6 @@ import com.flance.web.utils.GsonUtils;
 import com.google.common.collect.Lists;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.*;
 import org.springframework.util.StringUtils;
 
